aa8fb5b17a493a4665f6ccae56d3b1d9fd7dc7b8
codesnail <ahmedyaser@yahoo.com>
2018-10-07 18:25:18 -0500

    Add files via upload

---
+++ Vehicle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 22 15:38:37 2018
+
+@author: ahmed
+"""
+
+class Vehicle:
+    def __init__(self, bbox):
+        self.boundingBox = bbox
+        
+    
